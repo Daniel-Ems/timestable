3bb56130da86d09c8f19233a8c79fbd42f24ff64
Daniel Ems <danielems3@outlook.com>
2016-10-21 15:20:33 -0400

    edit comments from what i am doing to why i am doing them

--- timestable.c
+++ timestable.c
@@ -3,17 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-	//max_factor default
+	//set max_factor default.
 	int max_factor = 10;
 	
-	//min_factor default
+	//set min_factor default.
 	int min_factor = 1;
 	
-	//Seperates the beginning of the program from the cmd prompt
+	//Creates a clean appearance at the beginning of the program.
 	printf("\n");
 	
-	//The following three if statements evaluate the number of command line
-	//arguments the user passes, and sets variables accordingly.
+	//Ensure the user does not try and pass an "illegal" number of command
+	//line arguments. 
 	if(argc >= 4){
 		puts("***The program accepts 0, 1, or 2 arguments***");
 	}
@@ -25,19 +25,14 @@ int main(int argc, char *argv[])
 		max_factor = strtol(argv[1], NULL, 10);
 	}
 
-	//Defines valid values for max_factor as anything between 1 and 32 
-	//inclusively. max_factor is defaulted to 10 if an invalid value is 
-	//supplied, and the user is notified of the auto-correct, and reminded 
-	//of valid values.
+	//the conditions are used to error check for max_factors valid values.
 	if(max_factor <= 0 || max_factor > 32){
 		puts("*** Please keep your Maximum between 1 and 32 inclusively ***");
 		puts("*** Your Maximum has been auto-corrected to 10 ***\n");
 		max_factor = 10;
 	}
 	
-	//Defines valid values for min_factor as anything between 1 and Max_factor
-	//inclusively. min_factor has a default of 1, and is auto-corrected to such
-	//if the user supplies an invalid value. The user is then notified. 
+	//the condition are used to error check for min_factors valid values. 
 	if(min_factor <= 0 || min_factor > max_factor){
 		puts("*** Please keep your Minimum between 1 and the Maximum"
 		     "inclusively ***");
@@ -45,22 +40,21 @@ int main(int argc, char *argv[])
 		min_factor = 1;
 	}
 	
-	//Prints the times tables current min and max. 
+	//Allow the user to see what the printed max and min are. 
 	printf(">>>Times Table Minimum:%d, Maximum:%d", min_factor, max_factor);
 	printf("\n");
 	printf("%5s", "*");
 	
-	//Creates header row for times table 
+	//create a header for the table based on the default or user input.
 	for(int i = min_factor; i  <= max_factor; i++){
 		printf("%5d", i);
 	}
 	
-	//Creates output up and down.
+	//Allow for the table to be built with no arguments passed, one argument 
+	//passed, or two arguments passed. 
 	printf("\n");
 	for(int i = min_factor; i <= max_factor; i++){
 		printf("%5d", i);
-		
-		//Creates output left to right.
 		for(int j = min_factor; j <= max_factor; j++){
 			printf ("%5d", j * i);
 		}
