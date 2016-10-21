7d1a9d1aeec3c5939a8f580e7099c47759bab2f4
Daniel Ems <danielems3@outlook.com>
2016-10-21 12:04:51 -0400

    change variable name numbers to maximum for readability

--- timestable.c
+++ timestable.c
@@ -4,22 +4,22 @@
 
 int main(int argc, char *argv[])
 {
-	int numbers = 10;//max
+	int maximum = 10;//max
 	int minimum = 1;
 	
 	if(argc >=4){
 		puts("The program only accepts one argument");
 	}else if(argc == 3){
-		numbers = strtol(argv[2], NULL, 10);
+		maximum = strtol(argv[2], NULL, 10);
 		minimum = strtol(argv[1], NULL, 10);
 	}else if(argc == 2){
-		numbers = strtol(argv[1], NULL, 10);
+		maximum = strtol(argv[1], NULL, 10);
 	}else{
 		printf("10 X 10\n");	
 	}
 
-	if(numbers < 0){
-		numbers = 10;
+	if(maximum < 0){
+		maximum = 10;
 	}
 	if(minimum < 0){
 		minimum = 1;
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
 
 	printf("\n");
 	printf("%5s", "*");
-	for(int i = minimum; i  <=numbers; i++){
+	for(int i = minimum; i  <= maximum; i++){
 		printf("%5d", i);
 	}
 	printf("\n");
-	for(int i = minimum; i <= numbers; i++){
+	for(int i = minimum; i <= maximum; i++){
 		printf("%5d", i);
-		for(int j = minimum; j <= numbers; j++){
+		for(int j = minimum; j <= maximum; j++){
 			printf ("%5d", j * i);
 			}
 		printf("\n");
